6c61803740495d67d5955ef679c0c76bcf21f716
malinapavel <malina.pavel@student.upt.ro>
2023-05-22 20:35:32 +0300

    deleted HomeActivity page

--- app/src/main/java/com/example/iiotcamobileapp/NotificationActivity.java
+++ app/src/main/java/com/example/iiotcamobileapp/NotificationActivity.java
@@ -9,7 +9,7 @@ public class NotificationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notification);
         Intent intent = new Intent(this, ConfirmationActivity.class);
         startActivity(intent);
     }
